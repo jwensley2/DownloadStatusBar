f1de27616fa293ba68192806fa0d84fe6dae607a
Joseph Wensley <jwensley2@gmail.com>
2017-12-28 16:40:12 -0600

    Update Tooltip classes to avoid CSS conflicts
    Closes #34

--- src/tooltip/Tooltip.vue
+++ src/tooltip/Tooltip.vue
@@ -1,41 +1,41 @@
 <template>
-    <div id="DownloadStatusBarTooltip" class="tooltip" v-if="tooltipShown" :class="`theme-${theme}`" :style="{left: left}" :ref="'tooltip'">
-        <table>
-            <tr>
-                <th class="field-name">Filename:</th>
-                <td>{{ filename }}</td>
+    <div id="DownloadStatusBarTooltip" class="dsb-tooltip" v-if="tooltipShown" :class="`dsb-theme-${theme}`" :style="{left: left}" :ref="'tooltip'">
+        <table class="dsb-tooltip-table">
+            <tr class="dsb-tooltip-table-row">
+                <th class="dsb-tooltip-heading">Filename:</th>
+                <td class="dsb-tooltip-cell">{{ filename }}</td>
             </tr>
-            <tr>
-                <th class="field-name">Url:</th>
-                <td>{{ download.downloadItem.url }}</td>
+            <tr class="dsb-tooltip-table-row">
+                <th class="dsb-tooltip-heading">Url:</th>
+                <td class="dsb-tooltip-cell">{{ download.downloadItem.url }}</td>
             </tr>
-            <tr>
-                <th class="field-name">Referrer:</th>
-                <td>{{ download.downloadItem.referrer || 'None' }}</td>
+            <tr class="dsb-tooltip-table-row">
+                <th class="dsb-tooltip-heading">Referrer:</th>
+                <td class="dsb-tooltip-cell">{{ download.downloadItem.referrer || 'None' }}</td>
             </tr>
-            <tr>
-                <th class="field-name">Speed:</th>
-                <td>{{ downloadSpeed }}</td>
+            <tr class="dsb-tooltip-table-row">
+                <th class="dsb-tooltip-heading">Speed:</th>
+                <td class="dsb-tooltip-cell">{{ downloadSpeed }}</td>
             </tr>
-            <tr>
-                <th class="field-name">Status:</th>
-                <td>{{ status }}</td>
+            <tr class="dsb-tooltip-table-row">
+                <th class="dsb-tooltip-heading">Status:</th>
+                <td class="dsb-tooltip-cell">{{ status }}</td>
             </tr>
             <tr v-if="download.downloadItem.state !== 'complete'">
-                <th class="field-name">Progress:</th>
-                <td>{{ progress }}</td>
+                <th class="dsb-tooltip-heading">Progress:</th>
+                <td class="dsb-tooltip-cell">{{ progress }}</td>
             </tr>
             <tr v-else>
-                <th class="field-name">Filesize:</th>
-                <td>{{ filesize }}</td>
+                <th class="dsb-tooltip-heading">Filesize:</th>
+                <td class="dsb-tooltip-cell">{{ filesize }}</td>
             </tr>
-            <tr>
-                <th class="field-name">MIME type:</th>
-                <td>{{ download.downloadItem.mime || 'Unknown' }}</td>
+            <tr class="dsb-tooltip-table-row">
+                <th class="dsb-tooltip-heading">MIME type:</th>
+                <td class="dsb-tooltip-cell">{{ download.downloadItem.mime || 'Unknown' }}</td>
             </tr>
             <tr v-if="isImage">
-                <th class="field-name">Preview:</th>
-                <td><img class="preview" :src="download.downloadItem.url"></td>
+                <th class="dsb-tooltip-heading">Preview:</th>
+                <td class="dsb-tooltip-cell"><img class="dsb-preview" :src="download.downloadItem.url"></td>
             </tr>
         </table>
     </div>
@@ -49,7 +49,9 @@
 
     export default Vue.extend({
         name: 'tooltip',
-        props: ['theme'],
+        props: {
+            theme: String
+        },
         data(): {
             download: DSBDownload | null,
             element: HTMLElement | null,
@@ -163,53 +165,53 @@
         width      : auto;
         z-index    : 10;
 
-        table {
+        .dsb-tooltip-table {
             @include reset;
             max-width : 100%;
         }
 
-        tr {
+        .dsb-tooltip-table-row {
             @include reset;
 
-            + tr {
-                td, th { padding-top : 5px }
+            + .dsb-tooltip-table-row {
+                .dsb-tooltip-heading, .dsb-tooltip-cell { padding-top : 5px }
             }
         }
 
-        td, th {
+        .dsb-tooltip-heading, .dsb-tooltip-cell {
             @include reset;
             background      : light-theme("background");
             border-collapse : collapse;
             line-height     : 1.2;
         }
 
-        td {
+        .dsb-tooltip-cell {
             overflow      : hidden;
             padding-left  : 5px;
             text-overflow : ellipsis;
             word-break    : break-all;
         }
 
-        th {
+        .dsb-tooltip-heading {
             font-weight    : bold;
             text-align     : right !important;
             vertical-align : top;
             white-space    : nowrap;
         }
 
-        .preview {
+        .dsb-preview {
             max-width  : 300px;
             max-height : 200px;
             width      : auto;
             height     : auto;
         }
 
-        &.theme-dark {
+        &.dsb-theme-dark {
             background : dark-theme("background");
             border     : 1px solid dark-theme("border");
             color      : dark-theme("text");
 
-            th, td {
+            .dsb-tooltip-heading, .dsb-tooltip-cell {
                 background : dark-theme("background");
                 color      : dark-theme("text");
             }
